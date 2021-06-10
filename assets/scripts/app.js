c8ae222ad155d1fb3588c82b7f2cc1d6c0e71686
Thang Dang <dangthang013@gmail.com>
2021-06-10 11:08:29 +0700

    activate show log button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,13 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const LOG_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_MONSTER_ATTACK = 'MONSTER_ATTACK';
+const LOG_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
+const LOG_PLAYER_HEAL = 'PLAYER_HEAL';
+const LOG_GAME_OVER = 'GAME_OVER';
+
+
 const enteredNumber = prompt("Maximum life for you and the monster", "100");
 let maxLife = parseInt(enteredNumber);
 
@@ -13,10 +20,26 @@ let currentMonsterHealth = maxLife;
 let currentPlayerHealth = maxLife;
 let hasBonusLife = true;
 let initialHealth;
-
-
+let battleLog =[];
 
 adjustHealthBars(maxLife);
+
+function writeToLog(ev, val, playerHealth , monsterHealth, target)
+{
+    let entry= {
+        event: ev,
+        value: val,
+        target: target,
+        finalPlayerHealth:playerHealth,
+        finalMonsterHealth: monsterHealth
+    };
+    battleLog.push(entry);
+}
+
+function showLog()
+{
+    console.log(battleLog);
+}
 
 function reset()
 {
@@ -30,8 +53,11 @@ function endRound()
 {
     
     if(currentPlayerHealth > 0 && currentMonsterHealth <=0 )
+    {
         alert("YOU WON!!!");
-    else if(currentPlayerHealth <=0)
+        writeToLog(LOG_GAME_OVER, 'PLAYER WON', currentPlayerHealth, currentMonsterHealth,'');
+    }
+         else if(currentPlayerHealth <=0)
     {
         if(hasBonusLife)
         {
@@ -42,9 +68,16 @@ function endRound()
             setPlayerHealth(initialHealth)
         }
         else if(currentMonsterHealth>0)
+        {
             alert("xxxxx YOU LOST xxxxx");
+            writeToLog(LOG_GAME_OVER, 'MONSTER WON', currentPlayerHealth, currentMonsterHealth, '');
+        }
         else if(currentMonsterHealth<=0)
+        {
             alert("xxxxx WE HAVE A DRAW xxxxx");
+            writeToLog(LOG_GAME_OVER, 'A DRAW', currentPlayerHealth, currentMonsterHealth, '');                
+        }
+            
     }
     
     if(currentMonsterHealth<=0||currentPlayerHealth<=0){
@@ -55,18 +88,24 @@ function endRound()
 function attackMode(mode)
 {
     let maxDamage;
-
+    let logAttack;
     if(mode==='ATTACK')
-        maxDamage = ATTACK_VALUE
+    {
+        maxDamage = ATTACK_VALUE;
+        logAttack = LOG_PLAYER_ATTACK;
+    }
     else if(mode==='STRONG_ATTACK'){
-        maxDamage = STRONG_ATTACK_VALUE
+        maxDamage = STRONG_ATTACK_VALUE;
+        logAttack=LOG_PLAYER_STRONG_ATTACK;
     }
 
     let monsterDamage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= monsterDamage;
+    writeToLog(logAttack, monsterDamage, currentPlayerHealth, currentMonsterHealth, 'MONSTER');
     let playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     initialHealth = currentPlayerHealth;
     currentPlayerHealth -= playerDamage;
+    writeToLog(LOG_MONSTER_ATTACK, playerDamage, currentPlayerHealth, currentMonsterHealth, 'PLAYER');
     endRound();
 }
 
@@ -87,11 +126,12 @@ function healHandler()
         healValue =maxLife-currentPlayerHealth;
     else
         healValue= HEAL_VALUE;
-
+    writeToLog(LOG_PLAYER_HEAL, healValue, currentPlayerHealth, currentMonsterHealth, '');
     increasePlayerHealth(healValue);
     currentPlayerHealth+=healValue;
 }
 
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
-healBtn.addEventListener("click",healHandler)
+healBtn.addEventListener("click",healHandler);
+logBtn.addEventListener('click', showLog);
